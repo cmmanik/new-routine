7afa3acb572016b8d2ab94f41583b077c1a73444
cmmanik <mkmanik6523u@gmail.com>
2019-02-12 13:45:25 +0600

    setup all file

---
+++ index.js
@@ -0,0 +1,199 @@
+$(document).ready(function () {
+    var tabledata = [
+        {
+            id: 0,
+            ltGrd: 'A+',
+            minum: '87',
+            maxium: '95',
+            grpoint: '5'
+        },
+        {
+            id: 1,
+            ltGrd: 'B+',
+            minum: '50',
+            maxium: '60',
+            grpoint: '5'
+        },
+        {
+            id: 2,
+            ltGrd: 'A+',
+            minum: '80',
+            maxium: '90',
+            grpoint: '5'
+        },
+        {
+            id: 3,
+            ltGrd: 'F',
+            minum: '0',
+            maxium: '33',
+            grpoint: 'Fail'
+        }
+
+    ]
+
+    // Added data
+    function addData(obj) {
+        tabledata.push(obj);
+        return tabledata;
+    }
+
+    // edite data 
+    function editeData(newobj, indexd) {
+        tabledata[indexd] = newobj;
+        // console.log(tabledata);
+        return tabledata;
+    }
+
+    var idd = 202;
+    idd++;
+
+    var tabledatass = tabledata.map(ele => {
+        return (
+            `<tr id =" ${ele.id}" >
+            <th scope="row">${ele.ltGrd}</th>
+            <td>${ele.minum}</td>
+            <td>${ele.maxium}</td>
+            <td>${ele.grpoint}</td>
+            <td><button class="btn btn-primary editBtn" data-toggle="modal" data-target="#exampleModalCenter" >Edit</button></td>
+        </tr>`
+        );
+    })
+
+    $('.tblbdy').append(tabledatass);
+
+
+    // Added
+
+    $('#submitFrom').click(function (e) {
+        e.preventDefault();
+        var selctleter = "<option selected>Select Letter Grade</option>"
+        var ltGrade = $('#leterGrade').val();
+        var minimum = $('#mimimum').val();
+        var maximum = $('#maximum').val();
+        var grPoint = $('#grPoint').val();
+        if (minimum === "" || maximum === "") {
+            alert('Enter the correct information')
+
+        } else {
+            idd++;
+            var trData = `
+                <tr id ="${idd}" >
+                    <th scope="row">${ltGrade}</th>
+                    <td>${minimum}</td>
+                    <td>${maximum}</td>
+                    <td>${grPoint}</td>
+                    <td><button class="btn btn-primary editBtn" data-toggle="modal" data-target="#exampleModalCenter" >Edit</button></td>
+                </tr>
+            `;
+            $('.tblbdy').append(trData);
+
+            addData({
+                id: idd,
+                ltGrd: ltGrade,
+                minum: minimum,
+                maxium: maximum,
+                grpoint: grPoint
+            })
+            console.log(tabledata);
+        }
+
+        var ltGrade = $('#leterGrade').append(selctleter);
+        var minimum = $('#mimimum').val("");
+        var maximum = $('#maximum').val("");
+        var grPoint = $('#grPoint').val("");
+    });
+
+    // edite
+
+    $('.editBtn').on('click', function (e) {
+        console.log(e);
+
+        $('.my_from .form-group').each(function () {
+            $(this).remove();
+        })
+        // console.log(e.currentTarget.parentNode.parentNode.parentNode.rows);
+        var htmlColect = e.currentTarget.parentNode.parentNode.children;
+        var selectId = e.currentTarget.parentNode.parentNode;
+        var fid = (selectId.id);
+        var length = htmlColect.length - 1;
+        var dataArry = [];
+
+
+        var indx = tabledata.findIndex(ele => ele.id == fid);
+
+
+        for (var i = 0; i < length; i++) {
+            dataArry.push(htmlColect[i].innerText);
+        }
+        console.log(dataArry)
+        var ltrGrade = dataArry[0];
+        var modaldata = `
+             <div class="form-group">
+                              <label for="modalgrade">Letter Grade</label>
+                              <select class="custom-select form-control" id="modalgrade">
+                                  <option selected>${ltrGrade}</option>
+                                  <option value="A++">A++</option>
+                                  <option value="A+">A+</option>
+                                  <option value="A">A</option>
+                                  <option value="A-">A-</option>
+                                  <option value="B+">B+</option>
+                                  <option value="B-">B-</option>
+                                  <option value="B">B</option>
+                                  <option value="C">C</option>
+                                  <option value="C+">C+</option>
+                                  <option value="C-">C-</option>
+                                  <option value="D+">D+</option>
+                                  <option value="D-">D-</option>
+                                  <option value="D">D</option>
+                                  <option value="F">F</option>
+                              </select>
+                          </div>
+                          <div class="form-group">
+                              <label for="minimumnuber"> Minimum Number</label>
+                              <input type="text" value="${dataArry[1]}" class="form-control" id="minimumnuber">
+                          </div>
+                          <div class="form-group">
+                              <label for="maximum"> Maximum Number</label>
+                              <input type="text" value="${dataArry[2]}" class="form-control" id="maximum">
+                          </div>
+                          <div class="form-group">
+                              <label for="grtpoint"> Great Point</label>
+                              <input type="text" value="${dataArry[3]}" class="form-control" id="grtpoint">
+                          </div>
+                          `;
+
+        $('.my_from').append(modaldata);
+
+        $('#editSubmit').on('click',function (e) {
+            console.log(e);
+            e.preventDefault();
+            var objid = tabledata[indx].id;
+            console.log(tabledata[indx]);
+            alert(objid);
+            var ltrGrde = $('#modalgrade').val();
+            var minn    = $('#minimumnuber').val();
+            var max     = $('#grtpoint').val();
+            var gPoint  = $('#grtpoint').val();
+            var newobj  = {
+                id: objid,
+                ltGrd: ltrGrde,
+                minum: minn,
+                maxium: max,
+                grpoint: gPoint
+
+            }
+            editeData(newobj, indx)
+           // console.log(editeData(newobj, indx));
+            console.log(tabledata);
+            //  $('#modalgrade').val("");
+            // $('#minimumnuber').val("");
+            // $('#grtpoint').val("");
+            // $('#grtpoint').val("");
+
+        })
+
+
+    })
+
+
+})
